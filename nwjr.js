5948e020ed1b494adeed826321a2d035a4e7c8f8
Antriksh Yadav <antrikshy@gmail.com>
2015-03-25 13:09:22 -0700

    Improved error checking

--- nwjr.js
+++ nwjr.js
@@ -8,9 +8,18 @@ var MAC_EXECUTABLE = 'nwjs.app';
 var LINUX_EXECUTABLE = 'nw';
 var WIN_EXECUTABLE = 'nw.exe';
 
+var HELP_TEXT = "\nnwjr is a utility to quickly run and test NW.js apps during development.\n" +
+                "usage: nwjr (-s /path/to/NW.js/executables | /path/to/NW.js/app)\n" +
+                "   -s: Run in setup mode to copy and store NW.js files (run again to update NW.js)\n"
+
 var argv = require('minimist')(process.argv.slice(2));
 
 if (argv['s']) {
+    if (typeof argv['s'] === 'boolean') {
+        console.log(HELP_TEXT);
+        process.exit(-1);
+    }
+
     if (fs.existsSync(path.resolve(argv['s'], LINUX_EXECUTABLE)) || 
         fs.existsSync(path.resolve(argv['s'], MAC_EXECUTABLE)) ||
         fs.existsSync(path.resolve(argv['s'], WIN_EXECUTABLE))) {
@@ -25,16 +34,22 @@ if (argv['s']) {
     }
 
     else {
-        console.log("\nCould not find any NW.js executables in your directory, or that directory does not exist.\n");
+        console.log("Either that directory does not exist, or it's missing appropriate NW.js files.\n");
         process.exit(1);
     }
 }
 
 if (argv['_'].length == 1) {
+    // TODO: Find better way to check if folder exists
+    if (!fs.existsSync(path.resolve('.', 'executable'))) {
+        console.log("NW.js executables not found. Run setup using -s first.\n");
+        process.exit(-1);
+    }
+
     var pathToApp = path.resolve(argv['_'][0]);
 
     if (fs.existsSync(path.resolve(pathToApp, 'package.json'))) {
-        console.log("\nAttempting to launch NW.js app...\n");
+        console.log("Attempting to launch NW.js app...\n");
 
         if (process.platform == "darwin") {
             var nwjsExecLocation = path.resolve('.', 'executable', MAC_EXECUTABLE + '/Contents/MacOS/nwjs');
@@ -49,7 +64,12 @@ if (argv['_'].length == 1) {
     }
 
     else {
-        console.log("\nDirectory does not exist or package.json not found. Are you sure that's an NW.js app?\n");
+        console.log("Directory does not exist or package.json not found. Are you sure that's an NW.js app?\n");
         process.exit(1);
     }
+}
+
+else {
+    console.log(HELP_TEXT);
+    process.exit(-1);    
 }
